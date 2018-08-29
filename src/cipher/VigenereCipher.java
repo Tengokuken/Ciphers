966191b5cec759266540335b907087dafc3165d0
Johnson-Zhong <Johnsonz8642@gmail.com>
2018-08-29 14:05:01 -0400

    Added vignere cipher functionality

--- src/cipher/VigenereCipher.java
+++ src/cipher/VigenereCipher.java
@@ -1,23 +1,69 @@
 package cipher;
 
-public class VigenereCipher extends Cipher{
+//TODO: Javadocs, clean up code
+public class VigenereCipher extends Cipher {
 
   private String shift;
   
+  //TODO: Make a builder for the constructor, since it has optional params
   public VigenereCipher(String plainText, String encodedText, String shift) {
     super(plainText, encodedText);
     this.shift = shift;
-    // TODO Auto-generated constructor stub
   }
 
   @Override
   public void encode() {
-    // TODO Auto-generated method stub
+    String text = "";
+    // Loop through the input string
+    for (int i = 0; i < this.plainText.length(); i++) {
+      int shiftLoc = i;
+      int shiftChar = -1;
+      // Get the numerical value of the character
+      for (int j = 0; j < letters.length && (shiftChar == -1); j++) {
+        if (letters[j] == this.shift.charAt(shiftLoc))
+          shiftChar = j;
+      }
+      // Convert each letter in <input> to its integer form using <letters>
+      int convertedChar = -1;
+      // Get the integer form of the current letter
+      for (int j = 0; j < letters.length && (convertedChar == -1); j++) {
+        if (letters[j] == this.plainText.charAt(i))
+          convertedChar = j;
+      }
+      convertedChar = (convertedChar + shiftChar) % 26;
+      System.out.print(convertedChar);
+      char encodedChar = letters[convertedChar];
+      // Add the encoded character to a new string.
+      text += encodedChar;
+      System.out.println(text);
+    }
+    this.encodedText = text;
   }
 
   @Override
   public void decode() {
-    // TODO Auto-generated method stub
+    String text = "";
+    // Loop through the input string
+    for (int i = 0; i < this.encodedText.length(); i++) {
+      int shiftAmount = -1;
+      int shiftLoc = i;
+      // Get the numerical value of the character
+      for (int j = 0; j < letters.length && (shiftAmount == -1); j++) {
+        if (letters[j] == this.shift.charAt(shiftLoc))
+          shiftAmount = j;
+      }
+      // Convert each letter in <input> to its integer form
+      int convertedChar = -1;
+      for (int k = 0; k < letters.length && (convertedChar == -1); k++) {
+        if (letters[k] == this.encodedText.charAt(i))
+          convertedChar = k;
+      }
+      // Subtract <shift> from the <convertedChar> and convert it back to a char
+      char encodedChar = letters[((convertedChar - shiftAmount) % 26)];
+      // Add the encoded character to a new string.
+      text += encodedChar;
+    }
+    this.plainText = text;
     
   }
   public void setShift(String shift) {
--- src/driver/CipherDriver.java
+++ src/driver/CipherDriver.java
@@ -9,7 +9,8 @@ public class CipherDriver {
     // TODO: Singleton design, error-checking
     CipherTable.initializeCiphers();
     String cipher, type;
-    int shift;
+    // int shift;
+    String shift;
     Scanner in = new Scanner(System.in);
     System.out.print("Select your cipher: ");
     cipher = in.nextLine();
@@ -22,14 +23,19 @@ public class CipherDriver {
      * object ExistingCiphers.addCipher(input, desiredCipher); // Execute the
      * command } else { System.out.println("No"); } }
      */
-
+    // Create the Vigenere cipher with default parameters
+    VigenereCipher vigenere = new VigenereCipher("", "", "");
+    // Create the Caesar cipher with default parameters
+    //CaesarCipher caesar = new CaesarCipher("", "", 0);
     System.out.print("How much do you want to shift your input? ");
-    shift = Integer.parseInt(in.nextLine());
-    CaesarCipher caesar = new CaesarCipher("", "", shift);
+    //shift = Integer.parseInt(in.nextLine());
+    shift = in.nextLine();
     // TODO: Check if shift is a number
+    // TODO: Change this so shift comes after this. Requires big refactoring
     System.out.print("Do you want to encode or decode? ");
     type = in.nextLine();
     if (type.equals("encode")) {
+      /*
       System.out.print("Enter your text to encode: ");
       String text = in.nextLine();
       caesar.setplainText(text);
@@ -41,6 +47,26 @@ public class CipherDriver {
       caesar.setEncodedText(text);
       caesar.decode();
       System.out.println("Your decoded text: " + caesar.getPlainText());
+    } else {
+      // Give an error
+    }
+     */
+      System.out.print("Enter your text to encode: ");
+      String text = in.nextLine();
+      vigenere.setplainText(text);
+      vigenere.encode();
+      System.out.println("Your encoded text: " + vigenere.getEncodedText());
+    } else if (type.equals("decode")) {
+      System.out.print("Enter your text to decode: ");
+      String text = in.nextLine();
+      /*
+      caesar.setEncodedText(text);
+      caesar.decode();
+      System.out.println("Your decoded text: " + caesar.getPlainText());
+      */
+      vigenere.setEncodedText(text);
+      vigenere.decode();
+      System.out.println("Your decoded text: " + vigenere.getPlainText());
     } else {
       // Give an error
     }
